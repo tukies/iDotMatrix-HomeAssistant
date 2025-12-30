798f9b64ab88012657e02e75d8b881ab65013e03
Adrian Tukendorf <adrian.tukendorf@icloud.com>
2025-12-30 18:55:42 +0000

    Fix: Missing CONF_MAC import in __init__

--- custom_components/idotmatrix/__init__.py
+++ custom_components/idotmatrix/__init__.py
@@ -6,7 +6,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
+from .const import DOMAIN, CONF_MAC
 from .client.connectionManager import ConnectionManager
 
 _LOGGER = logging.getLogger(__name__)
